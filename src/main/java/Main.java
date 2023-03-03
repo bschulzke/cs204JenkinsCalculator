54b167347ecfaccba499b5e5804836a7ca54828e
Ben Schulzke <benschulzke@gmail.com>
2023-03-03 13:15:17 -0700

    break the pipeline, hopefully for the last time

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
   public static void main(String args[]) {
 
+    breaking the pipeline
+
     Calculator calculator = new Calculator();
 
     System.out.println("Welcome to the Calculator.");
